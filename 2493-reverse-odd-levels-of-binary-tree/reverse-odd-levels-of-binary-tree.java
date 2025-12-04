90482b9067999d69ffcbf03acbb04dc5842c03bf
Himanshu Tiwary <149292671+Himanshu2k24@users.noreply.github.com>
2025-12-04 19:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (45.76%) - LeetSync

---
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        reverseit(root.left,root.right,1);
+        return root;
+    }
+    private void reverseit(TreeNode left,TreeNode right,int idx){
+        if(left == null || right ==null) return;
+        if(idx % 2 == 1 ){
+            int  temp = left.val;
+            left.val = right.val;
+            right.val = temp;
+        }
+        reverseit(left.left,right.right,idx + 1);
+        reverseit(left.right,right.left,idx + 1);
+    }
+}
