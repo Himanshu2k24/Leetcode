4b19980211e71835b56c98b4640be4e393ae957f
Himanshu Tiwary <149292671+Himanshu2k24@users.noreply.github.com>
2025-12-09 10:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (53.86%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,17 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int[] dp = new int[n];
         Arrays.fill(dp,-1);
-        return helper(nums,0,dp);
-    }
+        dp[n - 1] = nums[n - 1];
+        dp[n- 2] = Math.max(dp[n-1],nums[n-2]);
 
-    private int helper(int[] nums,int i,int[] dp){
-        if( i >= nums.length ) return 0;
-        if(dp[i] != -1){
-            return dp[i];
+        for(int i = nums.length - 3; i>=0; i--){
+            dp[i] = Math.max(dp[i+2] + nums[i],dp[i+1]);
         }
-        int fromfirst = nums[i] + helper(nums,i+2,dp);
-        int fromsecond = helper(nums,i+1,dp);
-        return dp[i] = Math.max(fromfirst,fromsecond);
+        return dp[0];
     }
+   /* public int helper(int[] nums, int i,int[] dp){
+        if(i >= nums.length) return 0;
+        if(dp[i] != -1) return dp[i];
+        int first = nums[i] + helper(nums,i+2,dp);
+        int second = helper(nums,i+1,dp);
+        return dp[i] = Math.max(first,second);
+    }*/
 }
