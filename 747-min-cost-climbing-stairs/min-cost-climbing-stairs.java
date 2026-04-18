98edf96463381b9e169c754eb344c92fb2e5aa78
Himanshu Tiwary <149292671+Himanshu2k24@users.noreply.github.com>
2026-04-19 00:43:36 +0530

    Time: 1 ms (38.17%) | Memory: 45.2 MB (13.02%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,19 +1,20 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int n =cost.length;
+        int n = cost.length;
         int[] dp = new int[n];
         Arrays.fill(dp,-1);
-        int idx0 = helper(cost,0,dp);
-        int idx1 = helper(cost,1,dp);
-        return Math.min(idx0,idx1);
+        int first = helper(cost,0,dp);
+        int second = helper(cost,1,dp);
+        return Math.min(first,second); 
     }
-    private int helper(int[] cost,int i,int[] dp){
-        if(i > cost.length-1) return 0;
-        if(dp[i] != -1){
-            return dp[i];
-        }else{
-            return dp[i] = cost[i] + Math.min(helper(cost,i+1,dp),helper(cost,i+2,dp));
-        }
-        
+    public int helper(int[] cost,int idx,int[] dp) {
+        if(idx == cost.length -1) return cost[idx];
+        if(idx >= cost.length) return 0;
+
+        if(dp[idx] != -1) return dp[idx];
+        int first = cost[idx] + helper(cost,idx+1,dp);
+        int second = cost[idx] + helper(cost,idx + 2,dp);
+
+        return dp[idx] = Math.min(first,second);
     }
 }
